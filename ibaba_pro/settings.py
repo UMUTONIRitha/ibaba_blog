deb17731ab9376b4102de3096ecc9cef7139e34d
Zilfa Cyamani <zilcyam@gmail.com>
2021-05-08 12:22:59 +0200

    creating homepage

--- ibaba_pro/settings.py
+++ ibaba_pro/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ibaba',
         'USER': 'wecode',
-    'PASSWORD':'12345',
+    'PASSWORD':'123456',
     }
 }
 
